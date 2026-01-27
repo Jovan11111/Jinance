a51ec7f3881a3f0ec6d406e663e99add70e6712d
Jovan Janković <hongjongjo@gmail.com>
2026-01-27 18:34:25 +0100

    Added a class to encapsulate all Report data.

---
+++ models/report_information.py
@@ -0,0 +1,36 @@
+
+from models.earnings_information import EarningsInformation
+from models.news_article import NewsArticle
+from models.price_performance_information import PricePerformanceInformation
+
+class ReportInformation:
+    """Dataclass that represents all data that is supposed to be represented on the report."""
+
+    def __init__(self, earn_info: list[EarningsInformation], news: list[NewsArticle], price_perf: list[PricePerformanceInformation]):
+        self._earnings_information = earn_info
+        self._news = news
+        self._price_performance_information = price_perf
+        
+        
+    @property
+    def earnings_information(self) -> list[EarningsInformation]:
+        """Getter for earnings information."""
+        return self._earnings_information
+
+    @property
+    def news(self) -> list[NewsArticle]:
+        """Getter for news articles."""
+        return self._news
+
+    @property
+    def price_performance_information(self) -> list[PricePerformanceInformation]:
+        """Getter for price performance information."""
+        return self._price_performance_information
+
+    def to_dict(self) -> dict:
+        """Convert the ReportInformation object to dicttionary."""
+        return {
+            "earnings_information": self.earnings_information, # [earn.to_dict for earn in earnings_information]
+            "news": self.news, # [art.to_dict for art in news]
+            "price_performance_information": self.price_performance_information, # [performance.to_dict for performance in price_performance_information]
+        }
